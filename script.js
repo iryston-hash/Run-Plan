7e7c885417c3ff6cc386baa1ffe4af0aa4115de3
iryston <iryston58@gmail.com>
2022-08-14 15:03:18 +0200

    refactor , abstraction using classes

--- script.js
+++ script.js
@@ -22,11 +22,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
+
+
+class Workout{
+  date = new Date()
+  
+  constructor(coords, distance, duration) {
+    this.coords = coords
+    this.distance = distance
+    this.duration = duration
+  }
+}
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('could not get your location');
+        }
+      );
+    }
+  }
+  _loadMap(position) {
+    {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       // console.log(latitude, longitude);
@@ -34,66 +63,197 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
       //💾 Leaflet API
-      map = L.map('map').setView(coords, 13);
+      console.log(this);
+      this.#map = L.map('map').setView(coords, 13);
       // console.log(map);
-      L.tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      ).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+        this.#map
+      );
 
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-        // console.log(mapEvent);
-        // const { lat, lng } = mapEvent.latlng;
-        // L.marker([lat, lng])
-        //   .addTo(map)
-        //   .bindPopup(
-        //     L.popup({
-        //       maxWidth: 250,
-        //       minWidth: 100,
-        //       autoClose: false,
-        //       closeOnClick: false,
-        //       className: 'running-popup',
-        //     })
-        //   )
-        //   .setPopupContent('Location')
-        //   .openPopup();
-      });
-    },
-    function () {
-      alert('could not get your location');
+      this.#map.on('click', this._showForm.bind(this));
     }
-  );
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    // clear fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Location')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  // clear fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-    '';
+const app = new App();
 
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Location')
-    .openPopup();
-});
 
-inputType.addEventListener('change', function() {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       // console.log(latitude, longitude);
+//       // console.log(`https://www.google.com/maps/@${latitude},${longitude}13z`);
+
+//       const coords = [latitude, longitude];
+//       //💾 Leaflet API
+//       map = L.map('map').setView(coords, 13);
+//       // console.log(map);
+//       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+//         map
+//       );
+
+//       map.on('click', function (mapE) {
+//         mapEvent = mapE;
+//         form.classList.remove('hidden');
+//         inputDistance.focus();
+//         console.log(mapEvent);
+//         const { lat, lng } = mapEvent.latlng;
+//         L.marker([lat, lng])
+//           .addTo(map)
+//           .bindPopup(
+//             L.popup({
+//               maxWidth: 250,
+//               minWidth: 100,
+//               autoClose: false,
+//               closeOnClick: false,
+//               className: 'running-popup',
+//             })
+//           )
+//           .setPopupContent('Location')
+//           .openPopup();
+//       });
+//     },
+//     function () {
+//       alert('could not get your location');
+//     }
+//   );
+// }
+
+
+  //   this.#mapEvent = mapE;
+  //   form.classList.remove('hidden');
+  //   inputDistance.focus();
+  //   console.log(mapEvent);
+  //   const { lat, lng } = mapEvent.latlng;
+  //   L.marker([lat, lng])
+  //     .addTo(this.#map)
+  //     .bindPopup(
+  //       L.popup({
+  //         maxWidth: 250,
+  //         minWidth: 100,
+  //         autoClose: false,
+  //         closeOnClick: false,
+  //         className: 'running-popup',
+  //       })
+  //     )
+  //     .setPopupContent('Location')
+  //     .openPopup();
+
+
+
+
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   // clear fields
+//   inputDistance.value =
+//     inputDuration.value =
+//     inputCadence.value =
+//     inputElevation.value =
+//       '';
+
+//   console.log(mapEvent);
+//   const { lat, lng } = mapEvent.latlng;
+//   L.marker([lat, lng])
+//     .addTo(map)
+//     .bindPopup(
+//       L.popup({
+//         maxWidth: 250,
+//         minWidth: 100,
+//         autoClose: false,
+//         closeOnClick: false,
+//         className: 'running-popup',
+//       })
+//     )
+//     .setPopupContent('Location')
+//     .openPopup();
+// });
+
+// inputType.addEventListener('change', function () {
+//   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+//   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+// });
